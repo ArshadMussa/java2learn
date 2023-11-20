b4460d44c8939120f4ac55d44c1c7a68f4235213
Mussa, Arshad Mahomed <133218991+ArshadMussa@users.noreply.github.com>
2023-11-20 11:08:11 +0200

    Update HelloWorld.java

--- src/java/src/pt/c01basico/s00hello/HelloWorld.java
+++ src/java/src/pt/c01basico/s00hello/HelloWorld.java
@@ -2,6 +2,6 @@ package pt.c01basico.s00hello;
 
 public class HelloWorld {
   public static void main(String args[]) {
-	  System.out.println("O dinossauro pulou na lama.");
+	  System.out.println("Hello World./Olá Mundo.");
   }
 }
